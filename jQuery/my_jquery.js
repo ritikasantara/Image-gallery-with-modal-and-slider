768b4130d618f573fb119955b359ce264dba08c8
ritikasantara <rits.santara@gmail.com>
2016-08-27 23:22:47 +0530

    modal added

--- jQuery/my_jquery.js
+++ jQuery/my_jquery.js
@@ -3,19 +3,38 @@
 $(document).ready(function(){
 
 
-   $('figure img').click(function(){
+   //$('figure img').click(function(){
 
 
-      $('#photo_modal').modal();
+     //$('#photo_modal').modal();
 
-      $('.modal-body').load($(this).attr("src"));
+      //$('.modal-body').load($(this).attr("src"));
+
+
+   //});
+
+
+
+   //var modal = $("#photo_modal");
+   //var figure_modal_img = $("figure img");
+   //var modalImg= $("image_modal");
+   //var captionText = $(figure figcaption);
+
+   $('figure img').on('click',function(){
+
+        var src = $(this).attr('src');
+
+        var text = $(this).attr('alt');
+
+        $('#modal_image').attr('src',src);
+
+        $('#modal_caption').attr('alt',text);
+
+        $('#photo_modal').modal('show');
+
 
 
    });
-
-
-
-
 
 
 
